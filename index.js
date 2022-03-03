1ec80481e5ca832d546a45ffb32f3685fd34e9dc
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2022-03-03 01:20:08 -0600

    just some more psuedocode for index.js and  generateHTML.js

---
+++ index.js
@@ -0,0 +1,16 @@
+//should include prompts that are asked when application is started. answers are set to the properties in the different classes and subclasses
+
+//TODO: Include packages and files needed for this application
+  //packages: inquirer, fs
+  //file: generateHTML
+
+
+//TODO: questions array that includes objects with each question {type, name, message}
+
+//TODO: Create a function to write README file
+
+//TODO: Create a function to initialize app
+
+
+//TODO: Function call to initialize app 
+
---
+++ src/generateHTML.js
@@ -0,0 +1,6 @@
+//TODO: Create a function to generate the HTML for the html 
+
+//function generateHTML that return the html in a template literal ``
+
+
+//export the module 
