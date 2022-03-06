67fccd150b7180c7331ccc862bf29cc618e8d80f
Yelena Belova <42819374+kvs1995@users.noreply.github.com>
2022-03-06 04:42:30 -0600

    created helper variables to make function to create Cards a bit nicer looking

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,12 +1,51 @@
+//Create the unique html for each employeetype and the general addition
+const employeeHTMLStr = `
+      <div class="card">
+        <div class="card-header">
+          <h2>${data[i].name}</h2>
+          <h4>${data[i].constructor.name}</h4>
+        </div>
+        <div class="card-body">
+          <p id="id">ID: ${data[i].id}</p>
+          <p>Email: <a href="mailto: ${data[i].email}">${data[i].email}</a></p>
+`
+const endingTagsStr = `</div></div>`
+
+const managerHTMLStr = `<p>Office Number: ${data[i].officeNumber}</p>`
+
+const engineerHTMLStr = `<p>GitHub: <a href="https://www.github.com/${data[i].github}">${data[i].github}</a></p>`
+
+const internHTMLStr = `<p>School: ${data[i].school}</p>`
 
 //TODO: Create a function to generate the HTML for the html 
 function createCards(data) {
   let generatedCardString=``;
   for (let i = 0; i<data.length; i++) {
+    generatedCardString = generatedCardString + employeeHTMLStr
+    if (data[i].constructor.name === 'Manager') {
 
+    }
+    if (data[i].constructor.name === 'Engineer') {
+      
+    }
+    if (data[i].constructor.name === 'Intern') {
+      
+    }
   }
 }
-
+console.log(employeeList[0].constructor.name)
+/*       <div class="card">
+<div class="card-header">
+<h2>Name</h2>
+<h4>Job Title</h4>
+</div>
+<div class="card-body">
+<p id="id">ID</p>
+<p>Email: <a href="mailto: EMAIL">EMAIL</a></p>
+<p>Unique</p>
+</div>
+</div> 
+*/
 //function generateHTML that return the html in a template literal ``
 function generateHTML(data) {
   return `<!DOCTYPE html>
